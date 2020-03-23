a909a79cec6e4f0f2fbb57dd6a0ea771fc5cc206
Memhir-Yasue <eyasu.woldu@temple.edu>
2020-03-22 22:17:59 -0400

    stubs merged with routers

--- covid19_commcare_api/routers/community_inspections.py
+++ covid19_commcare_api/routers/community_inspections.py
@@ -2,3 +2,20 @@ from fastapi import APIRouter
 
 
 router = APIRouter()
+
+@router.get('/id')
+def get_id(id: int):
+    return 1
+
+@app.get('/GetAll')
+def get_all():
+    return 1
+
+@app.get('/')
+def get_record(LastRecord: int, PageSize: int):
+    return 1
+
+
+@app.get('/RRT')
+def get_rrt(Source: str, LastRecord: int, PageSize: int):
+    return 1
