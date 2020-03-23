db1dac4999ada0f77f2b7e9db21907661529d999
Memhir-Yasue <eyasu.woldu@temple.edu>
2020-03-22 23:45:41 -0400

    pass

--- covid19_commcare_api/routers/community_inspections.py
+++ covid19_commcare_api/routers/community_inspections.py
@@ -5,17 +5,17 @@ router = APIRouter()
 
 @router.get('/id')
 def get_id(id: int):
-    return 1
+    pass
 
 @app.get('/GetAll')
 def get_all():
-    return 1
+    pass
 
 @app.get('/')
 def get_record(LastRecord: int, PageSize: int):
-    return 1
+    pass
 
 
 @app.get('/RRT')
 def get_rrt(Source: str, LastRecord: int, PageSize: int):
-    return 1
+    pass
